3ddebddcec6b1a53cf020b525fc5fbed164e3784
GINTOAHC <chaotingchen10@gmail.com>
2024-04-28 18:05:20 +0800

    feat: Add register web page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import { ThemeProvider } from './components/providers/ThemeProvider'
+import Topbar from './components/Topbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <Topbar />
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,126 @@
-import { ModeToggle } from '@/app/components/providers/ThemeProvider'
+'use client'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+import { Button } from '@/components/ui/button'
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import emailjs from '@emailjs/browser'
+
+type PropT = {
+  dept: string
+  stuId: string
+  name: string
+  email: string
+}
+const SERVICE_ID = process.env.NEXT_PUBLIC_SERVICE_ID
+const TEMPLATE_ID = process.env.NEXT_PUBLIC_TEMPLATE_ID
+const PUBLIC_KEY = process.env.NEXT_PUBLIC_PUBLIC_KEY
+
+async function sendMail(prop: PropT) {
+  if (!SERVICE_ID || !TEMPLATE_ID || !PUBLIC_KEY) {
+    console.log(
+      'emailjs error: either SERVICE_ID, TEMPLATE_ID or PUBLIC_KEY is not set.'
+    )
+    return
+  }
+
+  const message = `dept: ${prop.dept}
+    stuId: ${prop.stuId}
+    name: ${prop.name}
+    email: ${prop.email}
+    `
+
+  try {
+    await emailjs.send(
+      SERVICE_ID,
+      TEMPLATE_ID,
+      {
+        message: message,
+        from: prop.email,
+        reply_to: prop.email,
+        reply_name: prop.name,
+      },
+      PUBLIC_KEY
+    )
+  } catch (err) {
+    console.log('emailjs error: ', err)
+  }
+
+  window.alert('Sended')
+}
+
+const FormSchema = z.object({
+  dept: z.string().min(2, {
+    message: 'Username must be at least 2 characters.',
+  }),
+  stuId: z.string().min(9),
+  name: z.string().min(2),
+  email: z.string().email(),
+})
+
+type FieldVal = { name: string; type: string; label: string; ph: string }
+const fields: FieldVal[] = [
+  { name: 'dept', type: 'text', label: '系級', ph: '資訊二' },
+  { name: 'stuId', type: 'text', label: '學號', ph: '110703000' },
+  { name: 'name', type: 'text', label: '姓名', ph: '王小明' },
+  { name: 'email', type: 'text', label: 'Email', ph: 'example@example.com' },
+]
 
 export default function Home() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      dept: '',
+    },
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log(data)
+    const prop = {
+      dept: data.dept,
+      stuId: data.stuId,
+      name: data.name,
+      email: data.email,
+    }
+    sendMail(prop)
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <ModeToggle />
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-2/3 space-y-6"
+        >
+          {fields.map((f) => {
+            return (
+              <FormField
+                key={f.name}
+                control={form.control}
+                name={f.name as any}
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>{f.label}</FormLabel>
+                    <FormControl>
+                      <Input placeholder={f.ph} type={f.type} {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            )
+          })}
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
     </main>
   )
 }
